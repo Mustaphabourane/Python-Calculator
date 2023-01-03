e9480f3edca005e6e7f24575d8d7308a1d85c5c3
Mustapha <91705614+Mustaphabourane@users.noreply.github.com>
2023-01-03 01:17:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,49 @@
 def add(a, b):
     answer = a + b
-    print(str(a) + " + " + str(b) + " = " + str(answer))
+    print(str(a) + " + " + str(b) + " = " + str(answer) + "\n")
 
 def sub(a, b):
     answer = a - b
-    print(str(a) + " - " + str(b) + " = " + str(answer))
+    print(str(a) + " - " + str(b) + " = " + str(answer) + "\n")
 
 
 def mul(a, b):
     answer = a * b
-    print(str(a) + " * " + str(b) + " = " + str(answer))
+    print(str(a) + " * " + str(b) + " = " + str(answer) + "\n")
 
 def div(a, b):
     answer = a / b
-    print(str(a) + " / " + str(b) + " = " + str(answer))
+    print(str(a) + " / " + str(b) + " = " + str(answer) + "\n")
 
+while True:
+    print("A. ADDITION")
+    print("B. SUBTRACTION")
+    print("C. MULTIPLICATION")
+    print("D. DIVISION")
+    print("E. exit")
 
-print("A. ADDITION")
-print("B. SUBTRACTION")
-print("C. MULTIPLICATION")
-print("D. DIVISION")
-print("E. exit")
+    choice = input("Input your choice: ")
 
-choice = input("Input your choice: ")
-
-if choice == "a" or choice == "A":
-    print("ADDITION")
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
-    add(a, b)
-elif choice == "b" or choice == "B":
-    print("SUBTRACTION")
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
-    sub(a, b)
-elif choice == "c" or choice == "C":
-    print("SUBTRACTION")
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
-    mul(a, b)
-elif choice == "d" or choice == "D":
-    print("SUBTRACTION")
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
-    div(a, b)
-elif choice == "e" or choice == "E":
-    print("programe ended")
-    quit()
+    if choice == "a" or choice == "A":
+        print("ADDITION")
+        a = int(input("input first number: "))
+        b = int(input("input second number: "))
+        add(a, b)
+    elif choice == "b" or choice == "B":
+        print("SUBTRACTION")
+        a = int(input("input first number: "))
+        b = int(input("input second number: "))
+        sub(a, b)
+    elif choice == "c" or choice == "C":
+         print("SUBTRACTION")
+         a = int(input("input first number: "))
+         b = int(input("input second number: "))
+         mul(a, b)
+    elif choice == "d" or choice == "D":
+         print("SUBTRACTION")
+         a = int(input("input first number: "))
+         b = int(input("input second number: "))
+         div(a, b)
+    elif choice == "e" or choice == "E":
+         print("programe ended")
+         quit()
